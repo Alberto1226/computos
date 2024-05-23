5da1d9961e1814551e5e8e5e7497b1c8510a4a2f
Alberto1226 <a15021347@iteshu.edu.mx>
2024-05-23 11:38:14 -0600

    migraciones

---
+++ database/migrations/2024_05_23_173435_create_casilla_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('casilla', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('id_seccion');
+            $table->text('tipoCasilla')->nullable();
+            $table->bigInteger('listaNominal')->nullable();
+            $table->bigInteger('votosNulos')->nullable();
+            $table->bigInteger('votosTotales')->nullable();
+            $table->text('ubicacion')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('casilla');
+    }
+};
---
+++ database/migrations/2024_05_23_173435_create_coaliciones_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('coaliciones', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('descripcion');
+            $table->string('id_partidos', 100)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('coaliciones');
+    }
+};
